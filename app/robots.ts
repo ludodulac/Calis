f3a8f335b730c2e9b9b668f57fd4e783882df439
ludodulac <162371616+ludodulac@users.noreply.github.com>
2026-08-30 18:48:02 +0200

    Fix GitHub Pages robots sitemap path

--- app/robots.ts
+++ app/robots.ts
@@ -2,9 +2,11 @@ import type { MetadataRoute } from "next";
 
 export const dynamic = "force-static";
 
+const siteUrl = "https://ludodulac.github.io/Calis";
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: "*", allow: "/" },
-    sitemap: "https://ludodulac.github.io/Calis/sitemap.xml",
+    rules: { userAgent: "*", allow: "/Calis/" },
+    sitemap: `${siteUrl}/sitemap.xml`,
   };
 }
